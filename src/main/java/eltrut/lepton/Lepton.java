aecd36241e8f87bc1cb6d12bbeb0307951de6202
Eltrut Likes <Eltrut>
2020-06-06 13:56:23 -0400

    Setup

---
+++ src/main/java/eltrut/lepton/Lepton.java
@@ -0,0 +1,29 @@
+package eltrut.lepton;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("lepton")
+public class Lepton
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Lepton() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
