c35afaec5a0dece541a6a97d9898026bf97b78dd
EltrutLikes <10eltrut1@gmail.com>
2021-01-12 22:51:15 -0500

    forge has a method for this now

--- src/main/java/eltrut/lepton/common/blocks/LogSlabBlock.java
+++ src/main/java/eltrut/lepton/common/blocks/LogSlabBlock.java
@@ -8,17 +8,10 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SlabBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
-
-//credit to SmellyModder(Luke Tonon)
+import net.minecraftforge.common.ToolType;
 
 public class LogSlabBlock extends SlabBlock {
 	private final Supplier<Block> block;
@@ -29,18 +22,9 @@ public class LogSlabBlock extends SlabBlock {
 	}
 	
 	@Override
-	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult result) {
-		ItemStack stack = player.getHeldItem(hand);
-		if (stack.getItem() instanceof AxeItem) {
-			world.playSound(player, pos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
-			if (!world.isRemote) {
-				world.setBlockState(pos, BlockUtil.transferAllBlockStates(state, this.block.get().getDefaultState()));
-				stack.damageItem(1, player, (playerIn) -> {
-					playerIn.sendBreakAnimation(hand);
-				});
-			}
-			return ActionResultType.SUCCESS;
-		}
-		return ActionResultType.PASS;
+	public BlockState getToolModifiedState(BlockState state, World world, BlockPos pos, PlayerEntity player, ItemStack stack, ToolType toolType) {
+		if (toolType == ToolType.AXE)
+			return BlockUtil.transferAllBlockStates(state, this.block.get().getDefaultState());
+		return super.getToolModifiedState(state, world, pos, player, stack, toolType);
 	}
 }
