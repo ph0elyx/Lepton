d46060c6ffa7f7a525343d5339cd93116330a71f
EltrutLikes <10eltrut1@gmail.com>
2021-02-06 21:58:12 -0500

    myalite but it's based and not cringe

--- src/main/java/eltrut/lepton/core/Lepton.java
+++ src/main/java/eltrut/lepton/core/Lepton.java
@@ -5,17 +5,13 @@ import org.apache.logging.log4j.Logger;
 
 import com.minecraftabnormals.abnormals_core.core.util.registry.RegistryHelper;
 
-import eltrut.lepton.client.ColorHelper;
 import eltrut.lepton.core.other.LeptonCompat;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("lepton")
@@ -32,7 +28,6 @@ public class Lepton
     public Lepton() {
         modEventBus.addListener(this::doCommonStuff);
     	modEventBus.addListener(this::doClientStuff);
-    	modEventBus.addListener(this::doLoadStuff);
         instance = this;
         
         REGISTRY_HELPER.register(modEventBus);
@@ -48,11 +43,7 @@ public class Lepton
     private void doClientStuff(final FMLClientSetupEvent event) {
     	event.enqueueWork(() -> {
         	LeptonCompat.registerBlockRenderers();
+        	LeptonCompat.registerBlockColors();
     	});
-    }
-    
-    @SuppressWarnings("deprecation")
-	private void doLoadStuff(final FMLLoadCompleteEvent event) {
-    	DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> ColorHelper.loadCompleteClient(event));
     }
 }
--- src/main/java/eltrut/lepton/core/other/LeptonCompat.java
+++ src/main/java/eltrut/lepton/core/other/LeptonCompat.java
@@ -1,14 +1,19 @@
 package eltrut.lepton.core.other;
 
+import java.util.Arrays;
+
+import com.minecraftabnormals.abnormals_core.core.util.DataUtil;
+
+import eltrut.lepton.client.MyaliteColorProvider;
 import eltrut.lepton.core.registry.LeptonBlocks;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.client.renderer.color.BlockColors;
+import net.minecraft.client.renderer.color.ItemColors;
 
 public class LeptonCompat {
 	
-	@OnlyIn(Dist.CLIENT)
 	public static void registerBlockRenderers() {
 		RenderTypeLookup.setRenderLayer(LeptonBlocks.GLOWSHROOM_HYPHAE_SLAB.get(), RenderType.getTranslucent());
 		RenderTypeLookup.setRenderLayer(LeptonBlocks.GLOWSHROOM_HYPHAE_STAIRS.get(), RenderType.getTranslucent());
@@ -19,6 +24,21 @@ public class LeptonCompat {
 		RenderTypeLookup.setRenderLayer(LeptonBlocks.STRIPPED_GLOWSHROOM_HYPHAE_STAIRS.get(), RenderType.getTranslucent());
 		RenderTypeLookup.setRenderLayer(LeptonBlocks.STRIPPED_GLOWSHROOM_HYPHAE_WALL.get(), RenderType.getTranslucent());
 		RenderTypeLookup.setRenderLayer(LeptonBlocks.STRIPPED_GLOWSHROOM_HYPHAE_VERTICAL_SLAB.get(), RenderType.getTranslucent());
+	}
+	
+	public static void registerBlockColors() {
+		BlockColors blockColors = Minecraft.getInstance().getBlockColors();
+		ItemColors itemColors = Minecraft.getInstance().getItemColors();
+		
+		DataUtil.registerBlockColor(blockColors, MyaliteColorProvider.getBlockColor(), Arrays.asList(
+				LeptonBlocks.POLISHED_MYALITE_BUTTON,
+				LeptonBlocks.POLISHED_MYALITE_PRESSURE_PLATE,
+				LeptonBlocks.POLISHED_MYALITE_WALL));
+		
+		DataUtil.registerBlockItemColor(itemColors, MyaliteColorProvider.getItemColor(), Arrays.asList(
+				LeptonBlocks.POLISHED_MYALITE_BUTTON,
+				LeptonBlocks.POLISHED_MYALITE_PRESSURE_PLATE,
+				LeptonBlocks.POLISHED_MYALITE_WALL));
 	}
 
 }
